15bb3b3d72ca609a8e651a7f12aa5dcb0d406211
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-06-16 20:04:49 +0300

    deployment errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
+	base: "/",
+	server: {
+		historyApiFallback: true,
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
 });
