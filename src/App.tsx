969610c94905fd19f6f752b7ea4460c6d6989362
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-06-16 03:00:54 +0300

    scrolling fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 /** @format */
 
 import { AnimatePresence } from "framer-motion";
-import { Routes, Route, useLocation } from "react-router-dom";
+import {
+	Routes,
+	Route,
+	useLocation,
+	BrowserRouter as Router,
+} from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { useState, useEffect } from "react";
 import Layout from "./components/layout/Layout";
@@ -10,6 +15,7 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Loading from "./components/ui/Loading";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 	const location = useLocation();
@@ -36,20 +42,23 @@ function App() {
 	}, [location.pathname]);
 
 	return (
-		<HelmetProvider>
-			<Layout>
-				{isLoading && <Loading />}
-				<AnimatePresence mode='wait'>
-					{isNavigating && <Loading />}
-					<Routes location={location} key={location.pathname}>
-						<Route path='/' element={<Home />} />
-						<Route path='/about' element={<About />} />
-						<Route path='/projects' element={<Projects />} />
-						<Route path='/contact' element={<Contact />} />
-					</Routes>
-				</AnimatePresence>
-			</Layout>
-		</HelmetProvider>
+		<Router>
+			<ScrollToTop />
+			<HelmetProvider>
+				<Layout>
+					{isLoading && <Loading />}
+					<AnimatePresence mode='wait'>
+						{isNavigating && <Loading />}
+						<Routes location={location} key={location.pathname}>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/projects' element={<Projects />} />
+							<Route path='/contact' element={<Contact />} />
+						</Routes>
+					</AnimatePresence>
+				</Layout>
+			</HelmetProvider>
+		</Router>
 	);
 }
 
